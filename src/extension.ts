244acab140ca754e13bb1fb4d8d5dc178b00a76c
msfukui <msfukui@gmail.com>
2018-11-10 23:04:41 +0900

    Added implementation to display [EOF] mark at end of the file.

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,57 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+"use strict";
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('"eof-mark" is activated.');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "eof-mark" is now active!');
+  const EofMarkDecorationType = vscode.window.createTextEditorDecorationType({
+    after: {
+      contentText: "[EOF]",
+      color: "gray"
+    }
+  });
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  let activeEditor = vscode.window.activeTextEditor;
+  if (activeEditor) {
+    updateDecorations();
+  }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+  vscode.window.onDidChangeActiveTextEditor(
+    editor => {
+      activeEditor = editor;
+      if (editor) {
+        updateDecorations();
+      }
+    },
+    null,
+    context.subscriptions
+  );
 
-    context.subscriptions.push(disposable);
-}
+  vscode.workspace.onDidChangeTextDocument(
+    event => {
+      if (activeEditor && event.document === activeEditor.document) {
+        updateDecorations();
+      }
+    },
+    null,
+    context.subscriptions
+  );
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+  function updateDecorations() {
+    if (!activeEditor) {
+      return;
+    }
+    const text = activeEditor.document.getText();
+    const Eof: vscode.DecorationOptions[] = [];
+
+    const startPos = activeEditor.document.positionAt(text.length - 1);
+    const endPos = activeEditor.document.positionAt(text.length);
+    const decoration = {
+      range: new vscode.Range(startPos, endPos),
+      hoverMessage: "End of file."
+    };
+    Eof.push(decoration);
+
+    activeEditor.setDecorations(EofMarkDecorationType, Eof);
+  }
 }
