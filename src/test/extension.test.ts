65ffcf66d48c408bd5663c8f4852648e5af4d8f4
msfukui <msfukui@gmail.com>
2018-11-11 22:03:21 +0900

    Impl extention tests.

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,15 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+import * as vscode from 'vscode';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function () {
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("[EOF] is displayed at the end of the file.", function() {
+        // Since it seems that it is unlikely that the current style
+        // of the document can be acquired, we decided to check only
+        // whether the test will work once.
+        let ae = vscode.window.activeTextEditor;
+        if (ae) {
+            assert.equal(ae.document.lineCount, 1);
+        }
     });
 });
